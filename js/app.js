21ef7221fa44785c8b4812be553c4d844a52c4a4
JacieWDI <janischan@hotmail.com>
2017-10-08 17:27:32 +0100

    Start planning

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-console.log('loaded');
+//PLAN - Whack a "mole"
+//Basic game: 10sec, start/reset buttons, display, counter, scoreboard
+//Additional 1: faster level(s)
+//Additional 2: two player mode player 1 first switch to player 2, compare --> winner
+
+//Sunday - review class notes/homeworks for logic required for basic game of 2x2, ideally get the basic working by lunchtime Monday
+//Monday PM - expand
+//Tuesday - Additional 1 / 2 time allowing & update READ.ME / start presentation info
+//Wednesday PM - Start visual & styling - graphics and sounds
+//Thursday - Complete Visual & styling
+//Friday - Present
+
+//PSEUDOCODE
+//BASIC GAME:
+//Start basic mechanics to work on 2x2 grid then expand
+
+//Step 1:
+//HTML 2x2 grid first
+
+//Start --> add click event to
+//Trigger COUNTER &
+//Trigger RANDOM display of 'moles'(squares) - "random li pick" for interval -->TIMEOUT
+
+//Step 2:
+//Click events attached to each random display mole - visual (and audio later)
+
+//Step 3:
+//Remove class and click after 1000
+
+//Step 4:
+//Loop
+
+//If above work then add in click event for HIT and hide plus increment score
+
+//Step 5:
+//If Else condition/statement for HIT --> knockout / to hide visually and increment score or
+//else NOT HIT --> disappear ()
+//__________________________________________
+//EXPANSION OF BOARD
+//___________________________________________
+
+//ADDITIONAL 1/2
+//Faster levels - change counter and loop counter
+
+
+//STYLING - visual & audio
+//Sports based - softball
